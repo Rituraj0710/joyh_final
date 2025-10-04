07ac92d549d4d5237dc384386a7447dad929e07d
Rituraj Gupta <rituraj.21scse1180185@galgotiasuniversity.edu.in>
2025-10-04 17:22:22 +0530

    AA

--- frontend/src/app/StoreProvider.jsx
+++ frontend/src/app/StoreProvider.jsx
@@ -1,14 +1,11 @@
 'use client'
 import { Provider } from 'react-redux'
 import { store } from '../lib/store';
-import { LanguageProvider } from '../context/LanguageContext';
 
 export default function StoreProvider({ children }) {
   return (
     <Provider store={store}>
-      <LanguageProvider>
-        {children}
-      </LanguageProvider>
+      {children}
     </Provider>
   )
 }
